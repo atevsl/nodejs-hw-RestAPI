1460355ba38a953e2414a255b32021bcc5739a26
atevsl <3380033@ukr.net>
2023-04-26 12:00:14 +0300

    part01

---
+++ routes/api/users.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const ctrl = require("../../controllers/users");
+
+const router = express.Router();
+
+const { validateBody } = require("../../middlewares");
+
+const { schemas } = require("../../models/user");
+
+router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
+
+module.exports = router;
